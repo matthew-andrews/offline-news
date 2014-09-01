da80f8463793c66bd5cc9e67bd3c654aa12d3fba
Matt Andrews <matt@mattandre.ws>
2014-09-01 22:23:06 +0100

    I don't want to have to teach regular expressions

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ var app = express();
 
 app.use(express.static(__dirname+'/public'));
 
-app.get(/^\//, function(req, res) {
+app.get('/article/:guid', bootstrap); 
+app.get('/', bootstrap);
+
+function bootstrap(req, res) {
   res.sendFile('public/index.html', { root: __dirname });
-});
+};
 
 app.listen(port);
 console.log('listening on '+port);
